8f869268a0077d9c50e9b3d058cd144eb5cdfc69
Emil Craciun <emil.craciun@live.com>
2016-01-29 22:13:20 +0200

    Added starting gulp packages and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/* File: gulpfile.js */
+
+// grab our gulp packages
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    jshint  = require('gulp-jshint'),
+    less    = require('gulp-less'),
+    path    = require('path');
+    //ts      = require('gulp-typescript');
+    
+    
